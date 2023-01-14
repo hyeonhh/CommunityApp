968721fc5efc6373dccb218bd2fdf18916891ef5
hyeon <tjdgustjdan@gmail.com>
2023-01-14 13:29:55 +0900

    navigation을 이용하여 하단 탭 만들기

--- app/src/main/java/com/example/mysoloapp/MainActivity.kt
+++ app/src/main/java/com/example/mysoloapp/MainActivity.kt
@@ -20,14 +20,19 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Firebase Auth
         auth = Firebase.auth
+//
+//        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+//            //로그아웃 버튼 클릭 시 로그아웃 시켜주기
+//            auth.signOut()
+//            val intent = Intent(this, IntroActivity::class.java)
+//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//            // 기존에 쌓인 액티비티들은 다 날려준다. -> 와이 ?
+//            startActivity(intent)
+//        }
 
-        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
-            //로그아웃 버튼 클릭 시 로그아웃 시켜주기
-            auth.signOut()
-            val intent = Intent(this, IntroActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            // 기존에 쌓인 액티비티들은 다 날려준다. -> 와이 ?
-            startActivity(intent)
-        }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/mysoloapp/SplashActivity.kt
+++ app/src/main/java/com/example/mysoloapp/SplashActivity.kt
@@ -4,18 +4,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import com.example.mysoloapp.auth.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
-        Handler().postDelayed(
-            { startActivity(Intent(this,IntroActivity::class.java))
-            finish() }
-            ,3000) //postDelayed
+        if(auth.currentUser?.uid==null){
+            //현재 유저의 uid 값이 없으면 로그인을 해 주어야하는 상황
+            Log.d("SplashActivity","null")
+            Handler().postDelayed(
+                { startActivity(Intent(this,IntroActivity::class.java))
+                    finish() }
+                ,3000) //postDelayed
+
+            // null이면 intro 엑티비티로 그대로 넘어간다.
+
+        }else{
+            Log.d("SplashActivity","not null")
+
+            Handler().postDelayed(
+                { startActivity(Intent(this,MainActivity::class.java))
+                    finish() }
+                ,3000) //postDelayed
+            // null이 아니면 바로 메인 엑티비티로 넘긴다.
+
+
+        }
+
+
+
 
     }//onCreate
 }
